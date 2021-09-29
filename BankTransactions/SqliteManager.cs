375b5c25cdbaddcc969d5aabfd8f158512bffe6b
Harrison Burt <hburt2003@gmail.com>
2021-09-29 17:17:48 +0100

    add sqlite manager

---
+++ BankTransactions/SqliteManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+namespace BankTransactions
+{
+    public class SqliteManager
+    {
+        private SqliteConnection _connection;
+
+        public SqliteManager()
+        {
+            _connection = new SqliteConnection(":memory:");
+
+            var cmd = _connection.CreateCommand();
+            cmd.CommandText =
+                "CREATE TABLE transactions (transaction_date TEXT, sender TEXT, recipient TEXT, description TEXT, amount INTEGER);";
+            cmd.ExecuteNonQuery();
+        }
+
+        public void AddTransaction(string date, string sender, string recipient, string desc, string amount)
+        {
+            
+        }
+    }
+}
